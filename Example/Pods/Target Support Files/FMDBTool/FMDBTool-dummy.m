5a15e2ffaea3df65aeead455f5a111456d14294c
liuli <lypcliuli@163.com>
2018-03-20 17:48:19 +0800

    清除项目

--- Example/Pods/Target Support Files/FMDBTool/FMDBTool-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FMDBTool : NSObject
-@end
-@implementation PodsDummy_FMDBTool
-@end
--- Example/Pods/Target Support Files/Pods-FMDBTool_Tests/Pods-FMDBTool_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FMDBTool_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_FMDBTool_Tests
-@end
